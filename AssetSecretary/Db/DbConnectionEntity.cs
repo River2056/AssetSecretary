4760113706e38e269352426b2118be8d10c833e4
kevin.tung <chen0625tung@gmail.com>
2021-09-07 19:51:51 +0800

    added sqlite db demo

---
+++ AssetSecretary/Db/DbConnectionEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AssetSecretary.Db
+{
+    public class DbConnectionEntity : DbContext
+    {
+        public DbSet<StockObject> daily_price { get; set; }
+        public string DbPath { get; set; }
+
+        public DbConnectionEntity()
+        {
+            DbPath = "C:\\Users\\user\\Desktop\\stocks.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+
+    public class StockObject
+    {
+        public int Id { get; set; }
+        public string Stock_Id { get; set; }
+        public string Stock_Name { get; set; }
+        public double Current_Price { get; set; }
+        
+        public override string ToString()
+        {
+            return $"id: {Id}, stock_id: {Stock_Id}, stock_name: {Stock_Name}, current_price: {Current_Price}";
+        }
+    }
+}
---
+++ AssetSecretary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AssetSecretary.Db;
+
+namespace AssetSecretary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DbConnectionEntity db = new DbConnectionEntity())
+            {
+                List<StockObject> list = db.daily_price.ToList();
+                foreach (StockObject s in list)
+                {
+                    Console.WriteLine(s.ToString());
+                }
+            }
+        }
+    }
+}
